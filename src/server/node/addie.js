26ab30c2bd4c462403e2deddaa45768e616b1eb7
zach-planet-nine <zach@planetnine.app>
2025-03-30 09:01:02 -0700

    fix

--- src/server/node/addie.js
+++ src/server/node/addie.js
@@ -43,7 +43,7 @@ console.log('b', bdoUUID);
       spellbooks
     };
 
-    if(!addie.fountUUID || !addie.bdoUUID || !spellbooks || spellbooks.length === 0) {
+    if(!addie.fountUUID || !addie.bdoUUID || !spellbooks) {
       throw new Error('bootstrap failed');
     }
 
