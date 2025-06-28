5a1290d3df2c1bf8a946f0f61f38f278902be457
zach-planet-nine <zach@planetnine.app>
2025-06-27 21:48:54 -0700

    subscription payment intent

--- src/server/node/src/processors/stripe.js
+++ src/server/node/src/processors/stripe.js
@@ -163,6 +163,75 @@ console.log('sending');
     return response;
   },
 
+  getSubscriptionPaymentIntent:  async (foundUser, amount, currency, subscriptionId, payees) => {
+    const customerId = foundUser.stripeCustomerId || (await stripeSDK.customers.create()).id;
+    if(foundUser.stripeCustomerId !== customerId) {
+      foundUser.stripeCustomerId = customerId;
+      await user.saveUser(foundUser);
+    }
+
+    const ephemeralKey = await stripeSDK.ephemeralKeys.create(
+      {customer: customerId},
+      {apiVersion: '2024-06-20'}
+    );
+
+    const groupName = 'group_' + foundUser.uuid;
+
+    const paymentIntent = await stripeSDK.paymentIntents.create({
+      amount: amount,
+      currency: currency,
+      customer: customerId,
+      setup_future_usage: 'off_session',
+      // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+      // is optional because Stripe enables its functionality by default.
+      automatic_payment_methods: {
+	enabled: true,
+      },
+      metadata: {
+        subscription_id: subscriptionId
+      },
+      transfer_group: groupName
+    });
+
+    let accountsAndAmounts = [];
+    for(var i = 0; i < payees.length; i++) {
+      const payee = payees[i];
+      const account = (await user.getUserByPublicKey(payee.pubKey)).stripeAccountId;
+      accountsAndAmounts.push({
+        account,
+        amount: payee.amount
+      });
+    }
+
+    const transferPromises = accountsAndAmounts.map(accountAndAmount => {
+      return stripeSDK.transfers.create({
+	amount: accountAndAmount.amount,
+	currency: 'usd',
+	destination: accountAndAmount.account,
+	transfer_group: groupName
+      });
+    });
+    await Promise.all(transferPromises);
+console.log('transferPromises');
+console.log('sending');
+    const response = {
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customerId,
+      publishableKey: stripePublishingKey
+    };
+
+    // No, let's do the payment intent, and then resolve the money splitting with MAGIC.
+    // We can store value on the user here, and resolve it with magic after creating the splits.
+
+    // let's websocket!
+  
+    // This needs to happen after the payment is confirmed...
+    return response;
+  },
+
+
+
   payPayees: async (payees, groupName, amount) => {
 console.log('payees', payees);
     const paidOutAmount = payees.reduce((a, c) => a + (c.minimumCost - c.minimumCost * 0.05), 0);
